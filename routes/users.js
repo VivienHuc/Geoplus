7f983824d89cda791280c08db66c22073e4438ac
Vivien HUC <vivien.prog@gmail.com>
2019-04-04 11:09:05 +0200

    fix

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 // Load User model
-const User = require('../models/user');
+const User = require('/models/User');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));

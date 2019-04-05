685d1150919d7dfbf58f78305748e1191c4d9f21
Vivien HUC <vivien.prog@gmail.com>
2019-04-05 10:44:14 +0200

    Add Habitant

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@ const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 var path = require("path");
 const fs = require('fs');
+var NodeGeocoder = require('node-geocoder');
+
+// NodeGeocoder Config
+var options = {
+  provider: 'google',
+
+  // Optional depending on the providers
+  httpAdapter: 'https', // Default
+  apiKey: 'AIzaSyBMFYX5JdYmfZbZZfVLZjPM5cTez1AlDeY', // for Mapquest, OpenCage, Google Premier
+  formatter: 'JSON'         // 'gpx', 'string', ...
+};
+var geocoder = NodeGeocoder(options);
 
 // Welcome Page
 router.get('/', (req, res) => res.render('welcome'));
@@ -43,4 +55,39 @@ router.post('/map', function(req, res) {
   }
 });
 
+// CRUD habitants
+router.get('/habitants', ensureAuthenticated, (req, res) =>
+res.render('habitants')
+);
+router.post('/habitants', ensureAuthenticated, (req, res) =>
+fs.readFile(__dirname + '/../views/js/db.json', (err, data) => {
+  var habitants;
+  if (err) throw err;
+  habitants = JSON.parse(data);
+  var i = habitants[0].id;
+  while (habitants[i]) {
+    i++;
+  }
+  var lat;
+  var lng;
+  geocoder.geocode(req.body.address, function(err, res) {
+    lat = res[0].latitude.toString(10);
+    lng = res[0].longitude.toString(10);
+    var new_habitant = {
+      id: i,
+      name: req.body.name,
+      address: req.body.address,
+      position: {
+        lat: lat,
+        lng: lng
+      },
+      given: "0"
+    };
+    habitants.push(new_habitant);
+    fs.writeFileSync(__dirname + '/../views/js/db.json', JSON.stringify(habitants));
+  })
+  res.redirect('/dashboard');
+})
+
+);
 module.exports = router;
